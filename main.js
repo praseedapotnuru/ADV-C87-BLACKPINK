9dd39358666c3df5338317ef7059795f5361bf31
praseedapotnuru <82453459+praseedapotnuru@users.noreply.github.com>
2022-01-14 17:48:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+// Create canvas variable
+var canvas=new fabric.Canvas('myCanvas');
+
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+	fabric.Image.fromURL(get_image, function(Img) {
+		block_image_object=Img;
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set( {
+			top:block_y, left:block_x
+		});
+		canvas.add(block_image_object);
+	});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') // add appropriate keycode
+	{
+		// upload red ranger
+		new_image('Rose.jpg');
+	}
+	if(keyPressed == '74')
+	{
+		// upload green ranger
+		new_image('jennie.jpg');
+	}
+	
+	if(keyPressed == '76')
+	{
+		block_x =520;
+		// upload yellow ranger
+		new_image('Lisa.jpg');
+	}
+	if(keyPressed == '69')
+	{
+		block_x = 800;
+		// upload pink ranger
+		new_image('Jisoo.jpg');
+	}
+}
+
